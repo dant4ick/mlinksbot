1915ba9449589bebba9bd7c906a7734330ba5f37
dant4ick <admin@dant4ick.ru>
2026-03-29 13:24:57 +0300

    add: configure proxy support for bot using AiohttpSession and ProxyConnector

--- shared.py
+++ shared.py
@@ -1,6 +1,12 @@
+import os
 from aiogram import Bot
 from aiogram.client.bot import DefaultBotProperties
+from aiogram.client.session.aiohttp import AiohttpSession
 from aiogram.enums import ParseMode
+from aiohttp_socks import ProxyConnector
 from config import API_TOKEN
 
-bot = Bot(token=API_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+PROXY_URL = os.environ.get("PROXY_URL", "socks5://shadowsocks:1080")
+
+session = AiohttpSession(connector=ProxyConnector.from_url(PROXY_URL))
+bot = Bot(token=API_TOKEN, session=session, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
